7df26c308bb279219f0c25858871e35d5d991a2c
delphidabbler <5164283+delphidabbler@users.noreply.github.com>
2022-11-16 12:58:48 +0000

    Update license boilerplate comments
    Update license from MPL 1.1 to MPL 2.0

--- Src/UBinaryVerInfo.pas
+++ Src/UBinaryVerInfo.pas
@@ -1,36 +1,14 @@
 {
-  Provides definition of class implementing of IVerInfoBinary and
-  IVerInfoBinaryReader interfaces that are exported from DLL. Also exports
-  function used to create objects supported by the DLL.
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at https://mozilla.org/MPL/2.0/
+ *
+ * Copyright (C) 2002-2022, Peter Johnson (https://gravatar.com/delphidabbler).
+ *
+ * Class implementing of IVerInfoBinary and IVerInfoBinaryReader interfaces that
+ * are exported from the DLL. Also defines a function used to create objects
+ * that are supported by the DLL.
 }
-
-
-{
- * ***** BEGIN LICENSE BLOCK *****
- *
- * Version: MPL 1.1
- *
- * The contents of this file are subject to the Mozilla Public License Version
- * 1.1 (the "License"); you may not use this file except in compliance with the
- * License. You may obtain a copy of the License at http://www.mozilla.org/MPL/
- *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
- * the specific language governing rights and limitations under the License.
- *
- * The Original Code is UBinaryVerInfo.pas
- *
- * The Initial Developer of the Original Code is Peter Johnson
- * (http://www.delphidabbler.com/).
- *
- * Portions created by the Initial Developer are Copyright (C) 2002-2004 Peter
- * Johnson. All Rights Reserved.
- *
- * Contributor(s):
- *
- * ***** END LICENSE BLOCK *****
-}
-
 
 unit UBinaryVerInfo;
 
